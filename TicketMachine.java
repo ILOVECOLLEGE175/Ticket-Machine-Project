8eb24d473dcc7a663998c663427629b1dd76fda0
Luca Casola <Lucapanther51@gmail.com>
2025-09-02 13:44:30 -0400

    Removed public from the machine and everything worked

--- TicketMachine.java
+++ TicketMachine.java
@@ -9,7 +9,7 @@
  * @author David J. Barnes and Michael Kölling
  * @version 7.1
  */
-class publicTicketMachine
+class TicketMachine
 {
     // The price of a ticket from this machine.
     private int price;
